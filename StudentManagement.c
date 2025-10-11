e339fc9b916cea7c9ee41b54b6a43cd3aff90197
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-10-11 23:30:02 +0700

    first commit

---
+++ StudentManagement.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+	char studentCode[15];
+	char fullName[255];
+	int birthYear;
+	char major[255];
+	float gpa;
+};
+
+void menu();
+void addStudent(struct Student students[], int *currentStudent);
+void displayAllStudent(struct Student students[], int currentStudent);
+void deleteStudent(struct Student students[], int *currentStudent);
+void clearEscape(char *str);
+void deleteStudent(struct Student students[], int *currentStudent);
+
+const int MAX_STUDENTS = 200;
+int currentStudent = 0;
+struct Student students[200];
+
+int main() {
+	int choice;
+	do {
+		menu();
+		scanf("%d", &choice);
+		getchar();
+		switch(choice) {
+			case 1:
+				displayAllStudent(students, currentStudent);
+				break;
+			case 2: 
+				addStudent(students, &currentStudent);
+				break;
+			case 3:
+				// chưa xử lý
+				editStudent(students, currentStudent);
+				break;
+			case 4: 
+				deleteStudent(students, &currentStudent);
+				break;
+			case 0: 
+//				deleteStudent(students, &currentStudent);
+				// exit
+				break;
+			default: 
+				printf("\nChoice invalid!\n");
+		}
+	} while(choice != 0);
+}
+
+// menu gọi học sinh
+void menu() {
+	printf("\n=========== Manager Student ===========");
+	printf("\n1. View all student");
+	printf("\n2. Add student");
+	printf("\n3. Edit student");
+	printf("\n4. Delete student");
+	printf("\n0. Exit!");
+	printf("\nPlease enter your choice: ");
+}
+
+// hàm nhập thông tin học sinh
+void addStudent(struct Student students[], int *currentStudent) {
+	if(*currentStudent >= MAX_STUDENTS) {
+		printf("\nThe number of students has reached the limit!");
+	} else {
+		struct Student student;
+		printf("===== Add Student =====");
+		printf("\nEnter Student Code: ");
+		scanf("%s", student.studentCode);
+		getchar();
+		
+		printf("Enter Full Name: ");
+		fgets(student.fullName, sizeof(student.fullName), stdin);
+		clearEscape(student.fullName);
+
+		
+		printf("Enter Birth Year: ");
+		scanf("%d", &student.birthYear);
+		getchar();
+		
+		printf("Enter Major: ");
+		fgets(student.major, sizeof(student.major), stdin);
+		clearEscape(student.major);
+
+		students[(*currentStudent)++] = student;
+		printf("\nAdd students successful!");
+	}
+}
+
+// hiển thị tất cả học sinh
+void displayAllStudent(struct Student students[], int currentStudent) {
+	if(currentStudent == 0) { 
+		printf("\nNo students to display!");
+	} else {
+		printf("\n=========== Dislay All Info Student ===========");
+		for(int i = 0; i < currentStudent; ++i) {
+			printf("\n=== Student %d ===", i + 1);
+			printf("\nStudent Code: %s", students[i].studentCode);
+			printf("\nFull Name: %s", students[i].fullName);
+			printf("\nBirth Year: %d", students[i].birthYear);
+			printf("\nMajor: %s", students[i].major);
+			printf("\n===============\n");
+		}
+	}
+}
+
+// xóa kí tự xuống dòng ở cuối chuỗi
+void clearEscape(char *str) {
+	str[strcspn(str, "\n")] = '\0';
+}
+
+// xóa học sinh
+void deleteStudent(struct Student students[], int *currentStudent) {
+	char studentCode[15];
+	if(*currentStudent == 0) { 
+		printf("\nNo students to delete!");
+	} else {
+		printf("Enter Student Code: ");
+		scanf("%s", studentCode);
+		// tìm vị trí của học sinh
+		int idx = findStudentByID(students, *currentStudent, studentCode);
+		if(idx != -1) {
+			for(int i = idx + 1; i < *currentStudent; ++i) {
+				students[i - 1] = students[i];
+			}
+			(*currentStudent)--;
+			printf("\nDelete students successful!");
+		} else {
+			printf("No students found!");
+		}
+	}	
+}
+int findStudentByID(struct Student students[], int currentStudent, char studentCode[15]) {
+	int idx = -1;
+	for(int i = 0; i < currentStudent; ++i) {
+		if(strcmp(students[i].studentCode, studentCode) == 0) {
+			idx = i;
+			break;
+		}
+	}
+	return idx;
+}
+
+// cập nhật sinh viên
+void editStudent(struct Student students[], int currentStudent) {
+	char studentCode[15];
+	struct Student student;
+	if(currentStudent == 0) { 
+		printf("\nNo students to edit!");
+	} else {
+		printf("Enter Student Code: ");
+		scanf("%s", studentCode);
+		getchar();
+		
+		// tìm vị trí của học sinh
+		int idx = findStudentByID(students, &currentStudent, studentCode);
+		if(idx != -1) {
+			
+			printf("===== Edit Student: %s =====", students[idx].fullName);
+			printf("\nEnter Full Name (Enter to skip): ");
+			fgets(student.fullName, sizeof(student.fullName), stdin);
+			clearEscape(student.fullName);
+	
+			printf("Enter Birth Year (Enter to skip): ");
+			scanf("%d", &student.birthYear);
+			getchar();
+			
+			printf("Enter Major: ");
+			fgets(student.major, sizeof(student.major), stdin);
+			clearEscape(student.major);
+			
+			strcpy(student.studentCode, students[idx].studentCode);
+			students[idx] = student;
+			
+			printf("Update student successfully!");
+		} else {
+			printf("No students found!");
+		}
+	}	
+}
